ce71a1b475b1c681e775a4b05a1a56d2836e2cba
maryambell <maryan.ahmedhaji@gmail.com>
2023-04-25 15:49:46 -0700

    lists.c

--- lists.c
+++ lists.c
@@ -91,7 +91,7 @@ size_t print_list_str(const list_t *h)
 		h = h->next;
 		i++;
 	}
-	return (i)
+	return (i);
 }
 
 /**
@@ -157,5 +157,3 @@ void free_list(list_t **head_ptr)
 	}
 	*head_ptr = NULL;
 }
-
-	
